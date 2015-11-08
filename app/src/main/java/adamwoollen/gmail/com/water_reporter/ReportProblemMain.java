388a20c64f088ecb5c3f51cb9601a02f1ceac778
CloCloCola <chloe.alsop@hotmail.co.uk>
2015-11-08 00:39:47 +0000

    report main page and submission hub

---
+++ app/src/main/java/adamwoollen/gmail/com/water_reporter/ReportProblemMain.java
@@ -0,0 +1,13 @@
+package adamwoollen.gmail.com.water_reporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportProblemMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_problem_main);
+    }
+}
---
+++ app/src/main/java/adamwoollen/gmail/com/water_reporter/ReportSubmitHub.java
@@ -0,0 +1,13 @@
+package adamwoollen.gmail.com.water_reporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportSubmitHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_submit_hub);
+    }
+}
